b80b8d1bb15aeb2fd24982f3bb6f074a0f149643
MdAfsarHossain <afsar291722@gmail.com>
2024-10-20 09:42:11 +0600

    Price Option jsx code added

---
+++ src/components/PriceOption/PriceOption.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import React from "react";
+import Feature from "../Feature/Feature";
+
+const PriceOption = ({ option }) => {
+  const { name, price, features } = option;
+
+  return (
+    <div className="bg-blue-500 rounded-md p-4 text-white mt-10 flex flex-col">
+      <h2 className="text-center">
+        <span className="text-7xl font-extrabold">{price}</span>
+        <span className="text-2xl">/mon</span>
+      </h2>
+      <h4 className="text-xl text-center my-8">{name}</h4>
+
+      <div className="flex-grow">
+        {features.map((feature, idx) => (
+          <Feature key={idx} feature={feature}></Feature>
+        ))}
+      </div>
+
+      <button className="mt-12 bg-green-600 w-full py-2 font-bold rounded-lg hover:bg-green-900">
+        Buy Now
+      </button>
+    </div>
+  );
+};
+
+PriceOption.propTypes = {
+  option: PropTypes.object.isRequired,
+};
+
+export default PriceOption;
